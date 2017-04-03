e18da327d7b276ca073acba2bfd1d1999f1f16ff
Arnaud Taffanel <arnaud@bitcraze.se>
2017-04-03 20:53:57 +0200

    Add camera input and ZMQ, starts to fly!

--- ledring-detector.py
+++ ledring-detector.py
@@ -5,69 +5,81 @@ import numpy as np
 
 import sys
 
+import zmq
+
 if len(sys.argv) < 2:
-    print("Usage: {} <image>".format(sys.argv[0]))
+    print("Usage: {} <camera_id>".format(sys.argv[0]))
     sys.exit(1)
 
-img = cv2.imread(sys.argv[1], cv2.IMREAD_GRAYSCALE)
-cv2.imshow('source', img)
+context = zmq.Context()
+socket = context.socket(zmq.PUSH)
+socket.bind("tcp://*:7777")
 
-ret, img = cv2.threshold(img, 150, 255, cv2.THRESH_BINARY)
-cv2.imshow('threshold', img)
+cap = cv2.VideoCapture(int(sys.argv[1]))
 
-# kernel = np.ones((2, 2), np.uint8)
-# img = cv2.erode(img, kernel)
-# cv2.imshow('erode', img)
-# cv2.dilate(img, kernel, iterations=1)
+while True:
+    ret, frame = cap.read()
+    img = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    cv2.imshow('source', img)
 
-# Detecting blob
-params = cv2.SimpleBlobDetector_Params()
+    ret, img = cv2.threshold(img, 150, 255, cv2.THRESH_BINARY)
+    cv2.imshow('threshold', img)
 
-params.filterByArea = True
-params.minArea = 1
-params.maxArea = 1000
+    # Detecting blob
+    params = cv2.SimpleBlobDetector_Params()
 
-params.filterByInertia = False
-params.filterByArea = False
-# params.filterByColor = False
-params.filterByConvexity = False
+    params.filterByArea = True
+    params.minArea = 1
+    params.maxArea = 1000
 
-detector = cv2.SimpleBlobDetector_create(params)
-keypoints = detector.detect(cv2.bitwise_not(img))
-imagePoints = []
-for point in keypoints:
-    imagePoints.append([point.pt[0], point.pt[1]])
-imagePoints = np.array(imagePoints)
-print(imagePoints)
-print(cv2.convexHull(imagePoints))
-img = cv2.drawKeypoints(img, keypoints, np.array([]), (0, 0, 255),
-                        cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
-cv2.imshow('keypoints', img)
+    params.filterByInertia = False
+    params.filterByArea = False
+    params.filterByConvexity = False
 
-# SolvePnP
-if len(imagePoints) == 4:
-    w = 0.02
-    objectPoints = [[0, w, 0],
-                    [-w, 0, 0],
-                    [w, 0, 0],
-                    [0, -w, 0]]
-    objectPoints = np.array(objectPoints)
-    cameraMatrix = np.array([[6.2321787645563745e+02, 0., 3.2242878461613770e+02],
-                             [0., 6.2360826877593502e+02, 2.4359896236415980e+02],
-                             [0., 0., 1.]])
-    distCoeffs = np.array([[1.1142910472083294e-01], [-2.6868764924280603e-01],
-                          [1.6666109149621208e-03], [2.3268549597284692e-03],
-                          [2.8967322731960432e-01]])
+    detector = cv2.SimpleBlobDetector_create(params)
+    keypoints = detector.detect(cv2.bitwise_not(img))
+    imagePoints = []
+    for point in keypoints:
+        imagePoints.append([point.pt[0], point.pt[1]])
+    imagePoints = np.array(imagePoints)
 
-    res, r, t = cv2.solvePnP(objectPoints, imagePoints,
-                             cameraMatrix, distCoeffs)
+    img = cv2.drawKeypoints(img, keypoints, np.array([]), (0, 0, 255),
+                            cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
+    cv2.imshow('keypoints', img)
 
-    if res:
-        print("Position: {}, {}, {}".format(t[0], t[1], t[2]))
+    # SolvePnP
+    if len(imagePoints) == 4:
+        w = 0.02
+        objectPoints = [[0, w, 0],
+                        [-w, 0, 0],
+                        [w, 0, 0],
+                        [0, -w, 0]]
+        objectPoints = np.array(objectPoints)
+        cameraMatrix = np.array([[6.2321787645563745e+02, 0., 3.2242878461613770e+02],
+                                 [0., 6.2360826877593502e+02, 2.4359896236415980e+02],
+                                 [0., 0., 1.]])
+        distCoeffs = np.array([[1.1142910472083294e-01], [-2.6868764924280603e-01],
+                              [1.6666109149621208e-03], [2.3268549597284692e-03],
+                              [2.8967322731960432e-01]])
+
+        res, r, t = cv2.solvePnP(objectPoints, imagePoints,
+                                 cameraMatrix, distCoeffs)
+
+        if res:
+            print("Position: {}, {}, {}".format(t[0][0], t[1][0], t[2][0]))
+            msg = {"pos": [t[0][0], t[1][0], t[2][0]], "detect": True}
+            socket.send_json(msg)
+        else:
+            print("SolvePnP failed!")
+            msg = {"pos": [0, 0, 0], "detect": False}
+            socket.send_json(msg)
     else:
-        print("SolvePnP failed!")
-else:
-    print("Cannot detect enough (4) points")
+        print("Cannot detect enough (4) points")
+        msg = {"pos": [0, 0, 0], "detect": False}
+        socket.send_json(msg)
 
-cv2.waitKey(0)
+    key = cv2.waitKey(1)
+    if key != 255:
+        break
+
 cv2.destroyAllWindows()
